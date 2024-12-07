eab44bc53419c037049de4966a347910bb135dbe
Azizbek <webcoderazizbek@gmail.com>
2024-12-07 16:58:50 +0500

    laravel

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\RegisteredUserController;
+
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\TudoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 
 
@@ -24,9 +24,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('register', [RegisterController::class, 'register']);
-Route::post('login', [LoginController::class, 'login']);
+Route::post('/register', [RegisterController::class, 'register']);
+ Route::post('login', [LoginController::class, 'login']);
 
 
 Route::middleware(['auth:sanctum'])->group( function(){
