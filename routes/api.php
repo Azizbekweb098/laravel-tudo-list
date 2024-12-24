12d072178dc20c8bf2344dacc18348fe237278ff
Azizbek <webcoderazizbek@gmail.com>
2024-12-24 17:23:22 +0500

    laravel

--- routes/api.php
+++ routes/api.php
@@ -24,17 +24,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [LoginController::class, 'login']);
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::resource('category', CategoryController::class);
-
     Route::resource('tudo', TudoController::class);
     Route::get('category_search', [SearchController::class, 'category']);
+    Route::resource('contact', ContactController::class); // To'g'irlandi
 });
-Route::resouce('contact', ContactController::class);
+
